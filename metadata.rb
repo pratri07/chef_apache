a033050b10f90fb6394d7470330bc5e0e09d1716
Prashant Atri <Pratri@Prashants-MacBook-Pro.local>
2017-05-02 19:53:05 -0400

    apache cookbook on centos65

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name 'apache'
+maintainer 'Prashant Atri'
+maintainer_email 'prashant_atri2003@yahoo.com'
+license 'all_rights'
+description 'Installs/Configures apache'
+long_description 'Installs/Configures apache'
+version '0.1.0'
